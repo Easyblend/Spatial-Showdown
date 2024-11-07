fe905da0a0224fc36c4c9f37137383eff79f4918
Emmanuel Kumah <emmanuel.kumah@kanop.io>
2024-11-07 21:56:17 +0100

    chore: folder restructing

---
+++ city-stealer/src/Component/Navbar.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+import { usePlayerContext } from '../Hooks/UsePlayerData';
+
+export default function Navbar({ setPage }) {
+  return (
+    <Box
+      zIndex={100}
+      sx={{ flexGrow: 1, backgroundColor: 'white' }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontFamily: 'retrogamer',
+              fontWeight: 700,
+              letterSpacing: 2,
+              textShadow: '-1px 0px #b31, 1px 0px #f3b;',
+            }}
+          >
+            City Stealer
+          </Typography>
+          <Stack direction="row">
+            <Button color="inherit">Creator</Button>
+            <Button
+              color="inherit"
+              onClick={() => setPage('technology')}
+            >
+              Technology
+            </Button>
+          </Stack>
+          <Button
+            color="success"
+            variant="contained"
+            onClick={() => setPage('websocket')}
+          >
+            Play
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
