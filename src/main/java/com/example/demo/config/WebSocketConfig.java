fc2b73f6e8dad495fbadd67d20c2809f40674e42
easyblend <kenzieemma072@gmail.com>
2024-11-07 21:43:18 +0100

    fix: allow cors

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://my-springboot-app-service.azurewebsites.net")  // Allow CORS for React app
+                .setAllowedOrigins("https://spatial-wars.netlify.app")  // Allow CORS for React app
                 .withSockJS(); // Enable fallback using SockJS
     }
 }
